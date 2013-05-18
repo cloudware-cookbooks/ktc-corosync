25b54ead7772d01584e2d9d1057d3251b0b26511
Robert <taeil.choi@kt.com>
2013-05-18 11:56:01 +0900

    remove 'backup' ruby_block

--- recipes/default.rb
+++ recipes/default.rb
@@ -66,24 +66,10 @@ template "/etc/cluster/cluster.conf" do
   only_if {node['corosync']['enable_openais_service'] == 'yes'}
 end
 
-# This block is not really necessary because chef would automatically backup thie file.
-# However, it's good to have the backup file in the same directory. (Easier to find later.)
-ruby_block "backup corosync init script" do
-  block do
-      original_pathname = "/etc/init.d/corosync"
-      backup_pathname = original_pathname + ".old"
-      FileUtils.cp(original_pathname, backup_pathname, :preserve => true)
-  end
-  action :create
-  notifies :create, "cookbook_file[/etc/init.d/corosync]", :immediately
-  not_if "test -f /etc/init.d/corosync.old"
-end
-
 cookbook_file "/etc/init.d/corosync" do
   source "corosync.init"
   owner "root"
   group "root"
   mode 0755
-  action :nothing
   notifies :restart, "service[corosync]", :immediately
 end
